b6c8588fd374fdee1a2b89bd35c77080841745e1
Maria Tauany Santos Feitosa <tauanysanttos13@gmail.com>
2024-01-03 00:39:37 -0300

    fix: correção de configurações não enviadas

--- AdaTech.ProjetoIndividual.GerenciadorTarefas/Models/Usuarios/DataUser/UsuarioData.cs
+++ AdaTech.ProjetoIndividual.GerenciadorTarefas/Models/Usuarios/DataUser/UsuarioData.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AdaTech.ProjetoIndividual.GerenciadorTarefas.Models.Usuarios
+namespace AdaTech.ProjetoIndividual.GerenciadorTarefas.Models.Usuarios.DataUser
 {
     internal class UsuarioData
     {
